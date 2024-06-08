adeb0672dac305a7b7026769847e7c2d8b3a882e
Piotr Z. Jelonek <piotr.z.jelonek@gmail.com>
2024-06-08 12:24:00 +0100

    Added class templates

---
+++ data_cleaning.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numps as np
+
+# class definition
+class DataCleaning:
+    """
+    This class will contain methods to clean data from each of the data sources.
+    """
+    # class constructor
+    def __init__(self):
+        pass
